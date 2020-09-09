0d0cbf040995f453420780325087f7841242dec6
Sreenivas K <sreenivask@Sreenivass-MacBook-Air.local>
2020-09-09 20:17:34 +0530

    initial commit

---
+++ ScreenUI/ContentView.swift
@@ -0,0 +1,22 @@
+
+//
+//  ContentView.swift
+//  ScreenUI
+//
+//  Created by Sreenivas K on 09/09/20.
+//  Copyright © 2020 Sreenivas K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
